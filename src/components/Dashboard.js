2ef8493db1be589a8fd20b0073b670394ead3a89
LukeDeBeneducci <beneducciluke@gmail.com>
2019-03-15 11:20:28 +0000

    server update

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -7,7 +7,7 @@ import io from 'socket.io-client';
 
 
 //const socket = io('http://localhost:6500');
-const socket = io('http://ldb-broadcasting.herokuapp.com:80')
+const socket = io('http://ldb-broadcasting-server.herokuapp.com:80')
 const pcConfig = {
 	iceTransports: 'relay',
 	'iceServers': [{
